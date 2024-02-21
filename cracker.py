b842b079dff589d2936d17ec95d697b21816c1bb
Voxl <84607110+stringpilot@users.noreply.github.com>
2024-02-22 10:27:45 +1100

    Create cracker.py
    MD5 Bruteforcer

---
+++ cracker.py
@@ -0,0 +1,38 @@
+import hashlib
+import argparse
+import os
+
+class HashConverter:
+    def __init__(self, passwordHashFileName, wordlistFileName):
+        self.passwordHashFileName = passwordHashFileName
+        self.wordlistFileName = wordlistFileName
+
+    def hash_strings(self, EncodedByteStringToHash):
+        hashAlgo = hashlib.md5(EncodedByteStringToHash)
+        return hashAlgo.hexdigest()
+
+    def crack(self):
+        passwordHashlist = []
+
+        with open(self.passwordHashFileName, 'rb') as passwordHashFile:
+            for passwHash in passwordHashFile:
+                passwordHashlist.append(passwHash.rstrip().decode())
+
+        with open(self.wordlistFileName, 'rb') as wordlistFile:
+            for bEncodedWord in wordlistFile:
+                bEncodedWord = bEncodedWord.rstrip()
+                EntryHash = self.hash_strings(bEncodedWord)
+
+                with open(self.passwordHashFileName, 'rb') as hashedPasswordFile:
+                    for hashed_password_entry in hashedPasswordFile:
+                        if EntryHash == hashed_password_entry.rstrip().decode():
+                            print("Password is:", bEncodedWord.decode(), "for Hash:", hashed_password_entry.decode())
+
+
+        if len(passwordHashlist) > 0:
+            for unidentified in passwordHashlist:
+                print("No password found:", unidentified)
+
+
+a1 = HashConverter("password-hashes.txt", "rockyou.txt")
+a1.crack()
